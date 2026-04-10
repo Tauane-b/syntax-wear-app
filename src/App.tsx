6bc9b4832e7e58570f2ec399c4083092a3a4c166
tauane Barros <tauane_aninha@hotmail.com>
2026-04-10 19:43:55 -0300

    feat: adiciona componente Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 function App() {
   return <>
     <Header />
+    <main className="py-10">
+    <Hero />
+    </main>
   </>;
 }
 
