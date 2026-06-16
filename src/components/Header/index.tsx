3e9b46a6ab31a50229928f6d6b86a4a61308ae1c
tauane Barros <tauane_aninha@hotmail.com>
2026-06-15 21:46:35 -0300

    feat: adiciona tela de nossas lojas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,13 @@
 import logo from "@/assets/images/logo.png";
 import IconUser from "@/assets/images/icon-user.png";
 import IconAbout from "@/assets/images/icon-about.png";
-import IconCart from "@/assets/images/icon-cart.png";
 import { Link } from "@tanstack/react-router";
 import { ShoppingCard } from "../ShoppingCard";
 
 export const Header = () => {
   return (
     <div className="relative">
-      <header className="fixed top-10 left-0 right-0 z-10 mx-10">
+      <header className="fixed top-5 left-0 right-0 z-10 mx-10">
         <div className="bg-white text-black max-w-[1320px] mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
           <Link to="/">
             {" "}
@@ -33,10 +32,10 @@ export const Header = () => {
           <nav>
             <ul className="flex gap-4 md:gap-10 ">
               <li className="hidden md:block">
-                <a href="#">Nossas Lojas</a>
+                <Link to="/our-stores">Lojas</Link>
               </li>
               <li className="hidden md:block">
-                <a href="#">Sobre</a>
+                <a href="#">Contato</a>
               </li>
               <li>
                 <a href="#">
@@ -48,7 +47,7 @@ export const Header = () => {
                   <img src={IconAbout} alt="Ícone de sobre" />
                 </a>
               </li>
-              
+
               <li>
                 <ShoppingCard />
               </li>
